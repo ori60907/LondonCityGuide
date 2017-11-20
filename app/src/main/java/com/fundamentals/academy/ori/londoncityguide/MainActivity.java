e9ff19b63f44384c6415c0c4b767c2dc28f7b426
Ori home computer <ori60907@gmail.com>
2017-11-20 18:21:59 +0200

    Initial commit

---
+++ app/src/main/java/com/fundamentals/academy/ori/londoncityguide/MainActivity.java
@@ -0,0 +1,13 @@
+package com.fundamentals.academy.ori.londoncityguide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
